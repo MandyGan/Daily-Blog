35423a2caf1884a1f7e8daefdacb371dd0757720
Lingling Gan <linglinggan@Linglings-MacBook-Pro.local>
2023-03-31 15:57:18 -0700

    add gitignore Procfile and node version

--- app.js
+++ app.js
@@ -71,8 +71,16 @@ app.get("/posts/:postId", function(req, res){
       });
     });
   });
-  
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+
+app.listen(process.env.PORT);
+  
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function(){
+  console.log("Server started on port 3000")
 });
+
+
